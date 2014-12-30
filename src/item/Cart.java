4adf2226ea31dab6b17691712e2e4c7288ff298f
shtao1 <953637941@qq.com>
2014-12-30 19:21:51 +0800

    new Version 1.2

--- src/item/Cart.java
+++ src/item/Cart.java
@@ -1,9 +1,14 @@
 package item;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cart {
     private ArrayList<Item> items = new ArrayList<Item>();
+
+    private static void add(Item item) {
+        Cart.add(item);
+    }
 
     public ArrayList<Item> getItems() {
         return items;
@@ -13,7 +18,19 @@ public class Cart {
         this.items = items;
     }
 
-    public void AddItem(Item item) {
-        items.add(item);
+    public void AddItem() {
+        Scanner in = new Scanner(System.in);
+        String[] temp;
+        StringBuilder inTemp = new StringBuilder();
+        do {
+            inTemp.append(in.nextLine());
+        } while (!inTemp.toString().replaceAll(" |　", "").endsWith("]"));
+        temp = inTemp
+                .toString()
+                .replaceAll("\\{|\\[|\\]| |　|\t|'|barcode|name|unit|price|discount|:|：", "")
+                .split("]，|},|}");
+        for (String a : temp) {
+            Cart.add(new Item(a.split(",|，")));
+        }
     }
 }
