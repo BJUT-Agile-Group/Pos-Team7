945eb10c5a4431ab63fc022913c6c95276be3f9d
LinkeyLeo <LinkeyLeo@outlook.com>
2014-12-30 14:38:43 +0800

    Product list

--- src/com/PosTeam7/Main.java
+++ src/com/PosTeam7/Main.java
@@ -1,9 +1,24 @@
 package com.PosTeam7;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello SOAP Again");
+        Scanner in = new Scanner(System.in);
+        ArrayList<String[]> product;
+        String[] temp;
+        StringBuilder inTemp = new StringBuilder();
+        do {
+            inTemp.append(in.nextLine());
+        } while (!inTemp.toString().endsWith("]"));
+        temp = inTemp.toString().replaceAll("\\{|\\[|\\]", "").replaceAll(" ", "").split("},|}");
+        ///
+        ///ewaogia
+        ///
+        for (String a : temp) {
+            System.out.println(a);
+        }
     }
 }
